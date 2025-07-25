9321225038c24c2fcf6617f33222e53efa577f0e
liruochen <liruochen@meituan.com>
2025-07-25 18:08:16 +0800

    learning

--- demo9.py
+++ demo9.py
@@ -1,8 +1,10 @@
+import os
+
 from dotenv import load_dotenv
 from langchain_tavily import TavilySearch
 
 load_dotenv(override=True)
 
-search = TavilySearch(max_retries=5)
+search = TavilySearch(tavily_api_key=os.getenv("TAVILY_API_KEY"))
 
 print(search.invoke("苹果2025WWDC发布会"))
